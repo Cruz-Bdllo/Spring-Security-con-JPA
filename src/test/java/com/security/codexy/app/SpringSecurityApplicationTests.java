a921d74925ec15758df9fa668e4941e1b9753813
Cruz Badillo <cubj25@gmail.com>
2020-05-05 11:50:02 -0500

    Primer commit: Estructura básica del proyecto (paquetes)

---
+++ src/test/java/com/security/codexy/app/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.codexy.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
