697b03eb32afcac25ad0403505fd165e20de2e20
Cruz Badillo <cubj25@gmail.com>
2020-05-05 21:53:14 -0500

    Diseño de la vista login, rutas personalizadas [/login, /logout] y mejoras en la lista de usuario (vista)

--- src/main/java/com/security/codexy/app/controllers/IndexController.java
+++ src/main/java/com/security/codexy/app/controllers/IndexController.java
@@ -1,11 +1,20 @@
 package com.security.codexy.app.controllers;
 
+import com.security.codexy.app.entities.User;
+import com.security.codexy.app.services.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private IUserService userService;
 
     @GetMapping(path = {"/"})
     public String index(Model model){
@@ -20,5 +29,16 @@ public class IndexController {
 
         return "error403";
     } // end of error
+
+    @RequestMapping("/login")
+    public String login(Model model, Principal dataUser){
+        model.addAttribute("title", "Inicia Sesión");
+
+        //User user = (dataUser != null) ? userService.findUserByEmail(dataUser.getName()) : null;
+
+        // prevenir el acceso al login cuando ya se inicio sesión
+        return (dataUser != null) ? "redirect:/" : "/login";
+    } // end login
+
 
 } // end controller
